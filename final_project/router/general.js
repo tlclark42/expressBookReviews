04f8d28ae70c478f3b95f3732eef07925d8a97e1
tlclark42 <tlclark42@gmail.com>
2024-11-18 14:16:34 -0500

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,9 +21,9 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
    // Extract the isbn parameter from the request URL
-   const email = req.params.isbn;
+   const isbn = req.params.isbn;
    // Filter the users array to find books whose isbn matches the extracted email parameter
-   let filtered_users = users.filter((user) => user.isbn === IntersectionObserver);
+   let filtered_users = users.filter((user) => user.isbn === isbn);
    // Send the filtered_users array as the response to the client
    res.send(filtered_users);
   //return res.status(300).json({message: "Yet to be implemented"});
@@ -32,19 +32,37 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+   // Extract the author parameter from the request URL
+   const author = req.params.author;
+   // Filter the users array to find books whose author matches the extracted email parameter
+   let filtered_users = users.filter((user) => user.author === author);
+   // Send the filtered_users array as the response to the client
+   res.send(filtered_users);
+ // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+   // Extract the author parameter from the request URL
+   const title = req.params.title;
+   // Filter the users array to find books whose title matches the extracted email parameter
+   let filtered_users = users.filter((user) => user.author === title);
+   // Send the filtered_users array as the response to the client
+   res.send(filtered_users);
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+   // Extract the review parameter from the request URL
+   const isbn = req.params.isbn;
+   // Filter the users array to find reviews whose isbn matches the extracted email parameter
+   let filtered_users = users.filter((user) => user.isbn === isbn);
+   // Send the filtered_users array as the response to the client
+   res.send(filtered_users);
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.general = public_users;
