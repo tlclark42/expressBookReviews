b04f53617c7897adc7c532cfe056247ef9f4c55b
tlclark42 <tlclark42@gmail.com>
2024-11-18 00:21:58 -0500

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,13 +13,20 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify({users}, null, 4));
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+   // Extract the isbn parameter from the request URL
+   const email = req.params.isbn;
+   // Filter the users array to find books whose isbn matches the extracted email parameter
+   let filtered_users = users.filter((user) => user.isbn === IntersectionObserver);
+   // Send the filtered_users array as the response to the client
+   res.send(filtered_users);
+  //return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
